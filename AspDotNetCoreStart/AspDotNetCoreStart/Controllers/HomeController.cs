ace1ac061d30296aa10541c6c3ecf8676d3b1008
Carl De Bels <crlDB@users.noreply.github.com>
2019-08-12 18:50:33 +0200

    -

--- AspDotNetCoreStart/AspDotNetCoreStart/Controllers/HomeController.cs
+++ AspDotNetCoreStart/AspDotNetCoreStart/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace AspDotNetCoreStart.Controllers
 {
@@ -13,5 +8,5 @@ namespace AspDotNetCoreStart.Controllers
         {
             return View();
         }
-   }
+    }
 }
