e978937bd3ce2f825a38fcf2137e1dad99324fb7
Carl De Bels <carl@nuyttensautomatisatie.be>
2019-08-12 15:56:04 +0200

    -

---
+++ AspDotNetCoreStart/AspDotNetCoreStart/Src/js/App.js
@@ -0,0 +1,15 @@
+"use strict";
+//! App
+//! version : 1.0.0
+//! author  : De Bels Carl
+//! license : MIT
+//! date    : Q1-2019
+Object.defineProperty(exports, "__esModule", { value: true });
+require("../img/core2.0.png");
+require("../css/site.css");
+require("../../node_modules/bootstrap/dist/css/bootstrap.css");
+$('main')
+    .append('<p>JQuery loaded</p>');
+$('main')
+    .append('<button type="button" class="btn btn-primary">Bootstrap loaded</button>');
+//# sourceMappingURL=App.js.map
